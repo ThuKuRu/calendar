492a6529e3ab5f68a8c2f17f9688703c0c52b5cc
Musgra <musmus2609@gmail.com>
2023-06-04 22:04:20 +0700

    ko bik noi gi lun a

--- src/containers/Layout/Main/index.style.js
+++ src/containers/Layout/Main/index.style.js
@@ -5,54 +5,13 @@ const Content = styled.div`
   flex-direction: column;
   height: 100%;
   width: 100%;
-  .nav {
-    height: 30px;
-  }
-  .navigate {
-    height: 30px;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 4px 0px;
-    background-color: #fff;
-    .navigateLeft {
-      margin-left: 20px;
-      display: flex;
-      .nav {
-        background-color: #3f80ea;
-        color: #fff;
-        font-size: 16px;
-        border-radius: 5px;
-        margin-left: 2px;
-        border: none;
-        padding: 5px 10px;
-      }
-    }
-    .navigateRight {
-      display: flex;
-      margin-left: 850px;
-      margin-right: 20px;
-      .nav {
-        background-color: #3f80ea;
-        border-radius: 5px;
-        margin-left: 2px;
-        border: none;
-        padding: 5px 10px;
-        border: none;
-        color: #fff;
-        cursor: pointer;
-        a {
-          color: #fff;
-          font-size: 16px;
-          text-decoration: none;
-        }
-      }
-    }
-  }
   .content {
     display: flex;
     height: 100%;
     padding: 10px 10px;
+    .container {
+      width: 100%;
+    }
   }
 `;
 
