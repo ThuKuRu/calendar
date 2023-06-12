ac09aa955d66d4cc5d552ecd9b1fc868b1b568bd
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-06-12 23:26:45 +0700

    fix import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./containers/Home/Home";
 import Login from "./containers/Login/Login";
 import Register from "./containers/Register/Register";
-import Dashboard from "./containers/Dashboard/Dashboard";
 
 function App() {
   const router = createBrowserRouter([
