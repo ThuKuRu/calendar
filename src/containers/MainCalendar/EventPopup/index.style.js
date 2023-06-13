412c59b7f3b17f7a1c58f0af8aebf145f42bb90b
Hoangpv3 <hoang.pv208000@sis.hust.edu.vn>
2023-06-13 22:00:34 +0700

    Sua popup

--- src/containers/MainCalendar/EventPopup/index.style.js
+++ src/containers/MainCalendar/EventPopup/index.style.js
@@ -1,38 +1,38 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  .popup{
-    height: 467px;
-    width: 420px;
+  .popup {
+    height: 40vh;
+    width: 20vw;
     display: flex;
     flex-direction: column;
     background-color: #ffffff;
     border-radius: 10px;
     font-family: "Roboto";
-    border: 1px solid #EFF0F6;
+    border: 1px solid #eff0f6;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    .colorHeading{
-        background-color: #2D7FE0;
-        height: 44px;
-        width: 100%;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
+    .colorHeading {
+      background-color: #2d7fe0;
+      height: 44px;
+      width: 100%;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
     }
-    .content{
+    .content {
+      display: flex;
+      padding: 10px;
+      height: 100%;
+      flex-direction: column;
+      .header {
+        color: #303f9f;
+        font-size: 27px;
+        font-weight: semi-bold;
         display: flex;
-        padding: 10px;
-        height: 100%;
-        flex-direction: column;
-        .header{
-            color: #303F9F;
-            font-size: 27px;
-            font-weight: semi-bold;
-            display: flex;
-            margin-bottom: 10px;
-        }
-        .time{
-            margin-bottom: 10px;
-        }
+        margin-bottom: 10px;
+      }
+      .time {
+        margin-bottom: 10px;
+      }
     }
   }
 `;
