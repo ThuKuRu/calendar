ac3801b5005206372aac3a4ceaa9b283fd8e3b9a
TuanAnh225 <tuananhchums23@gmail.com>
2023-06-16 00:14:06 +0700

    Dash update

--- src/containers/Dashboard/index.style.js
+++ src/containers/Dashboard/index.style.js
@@ -2,79 +2,122 @@ import styled from "styled-components";
 
 const DashBoard = styled.div`
   .container {
-    display: flex;
-    flex-direction: column;
-    .top-container {
-      .miniCal {
-        margin-left: 5%;
+    display: grid;
+    grid-template-columns: 35% 65%;
+    gap: 1em 2em;
+    padding: 0.5em 2em 0 2em;
+
+    .upComingTask {
+      background-color: #ffcdff;
+      border-radius: 36px;
+      border: 1px solid #b7b7b7;
+      box-shadow: 1px 5px 5px #aaaa;
+      .upComingTask-top {
+        display: flex;
+        justify-content: space-between;
+        margin: 0.5em 0.7em;
+        .taskStatus {
+          font-size: claim(0.75rem, 1.25rem, 3rem);
+          background-color: #2d7fe0;
+          color: white;
+          width: 50%;
+          border-radius: 12px;
+          padding: 5px 10px;
+          text-align: center;
+        }
+        .upComingTaskDate {
+          align-items: center;
+        }
+      }
+      .upComingTaskNoti {
+        margin-left: 0.5em;
+        .upComingTaskNotify {
+          display: flex;
+          align-items: center;
+          width: 60%;
+          margin-top: 1.25rem;
+          font-size: claim(0.75rem, 1.25rem, 3rem);
+          img {
+            border-radius: 50%;
+            width: 2em;
+            margin-right: 3%;
+          }
+        }
+        .upComingTaskNotifyDes {
+          width: 60%;
+          margin-top: 0.625rem;
+          font-size: claim(0.75rem, 0.75rem, 3rem);
+        }
+      }
+
+      .upComingTaskMember {
+        display: flex;
+        flex-direction: row;
+        margin: 1.6rem 0 1.6rem 0.5rem;
+
+        img {
+          border-radius: 50%;
+          width: 2em;
+          height: 2em;
+          margin-right: 3%;
+          fon-size: 1.5rem;
+        }
+        .afterImg {
+          margin-left: -5%;
+        }
+        .numberMore {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 2.25rem;
+          height: 2.25rem;
+          background-color: #d9d9d9;
+          border-radius: 50%;
+          font-weight: bold;
+          font-size: claim(0.75rem, 1.25rem, 3rem);
+          margin-left: -5%;
+        }
       }
     }
-    .bottom-container {
-      .upComingTask {
-        width: 30%;
-        background-color: #ffcdff;
-        margin-left: 5%;
-        margin-top: 3%;
-        padding: 10px 20px;
-
+    .bottomRightDashContainer {
+      display: grid;
+      grid-template-columns: 45% 45%;
+      gap: 1em 2em;
+      padding: 0.5em 2em 0 2em;
+      .upNextComingTask {
+        background-color: #76bdff;
+        border-radius: 36px;
+        border: 1px solid #b7b7b7;
+        box-shadow: 1px 5px 5px #aaaa;
         .upComingTask-top {
           display: flex;
           justify-content: space-between;
+          margin: 0.5em 0.7em;
           .taskStatus {
+            font-size: claim(0.75rem, 1.25rem, 3rem);
             background-color: #2d7fe0;
             color: white;
-            width: 30%;
+            width: 50%;
             border-radius: 12px;
             padding: 5px 10px;
             text-align: center;
           }
           .upComingTaskDate {
             align-items: center;
-            font-size: 20px;
           }
         }
-        .upComingTaskNotify {
-          display: flex;
-          align-items: center;
-          width: 60%;
-          margin-top: 15%;
-          font-size: 20px;
-          img {
-            border-radius: 50%;
-            width: 35px;
-            margin-right: 3%;
-          }
-        }
-        .upComingTaskNotifyDes {
-          margin-top: 5%;
-        }
-        .upComingTaskMember {
-          margin-top: 20%;
-          margin-bottom: 5%;
-          display: flex;
+        // .upComingTaskNotify {
+        //   display: flex;
+        //   align-items: center;
+        //   width: 60%;
 
-          img {
-            border-radius: 50%;
-            width: 35px;
-            height: 35px;
-            margin-right: 3%;
-          }
-          .afterImg {
-            margin-left: -5%;
-          }
-          .numberMore {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 35px;
-            height: 35px;
-            background-color: #d9d9d9;
-            border-radius: 50%;
-            font-weight: bold;
-            font-size: 20px;
-            margin-left: -5%;
-          }
-        }
+        //   font-size: claim(0.75rem, 1.25rem, 3rem);
+        //   img {
+        //     border-radius: 50%;
+        //     width: 16px;
+        //     margin-right: 3%;
+        //   }
+        // }
       }
     }
   }
