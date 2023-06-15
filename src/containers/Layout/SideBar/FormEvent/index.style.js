8e9bafbefd664b421213f6d0c08099cf3729f355
nguyenbahoang02 <nguyenbahoang02@gmail.com>
2023-06-16 00:34:16 +0700

    update responsive for

--- src/containers/Layout/SideBar/FormEvent/index.style.js
+++ src/containers/Layout/SideBar/FormEvent/index.style.js
@@ -14,9 +14,7 @@ const FormEventStyle = styled.div`
     align-items: center;
     display: flex;
     .modal-container {
-      width: 35%;
-      max-width: 650px;
-      height: auto;
+      width: clamp(27.5em, 35%, 40em);
       border: 1.5px solid #011627;
       border-radius: 15px;
       background-color: #fdfffc;
@@ -26,46 +24,41 @@ const FormEventStyle = styled.div`
       color: #011627;
 
       .header {
-        height: 100px;
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: 5px;
-        .headerText {
-          font-size: 32px;
-        }
+        padding: 1em;
+        font-size: clamp(1.6rem, 1.6vw, 2rem);
       }
 
       .content {
         width: 80%;
-        padding: 10px 5px;
-        font-size: 20px;
+        padding: 0.5em 0.25em;
+        font-size: clamp(1rem, 1vw, 1.25rem);
 
         .add-form {
-          height: 30px;
           width: 95%;
-          margin-bottom: 10px;
+          margin-bottom: 0.55em;
           border: 1px solid #000000;
           border-radius: 15px;
-          padding: 8px 12px;
-          font-size: 18px;
-          font-family: "Roboto";
+          padding: 0.44em 0.66em;
+          font-size: clamp(0.9375rem, 0.9375vw, 1.125rem);
+          font-family: inherit;
         }
         .formCreate-container {
           .formCreate-container-things {
             display: flex;
             justify-content: space-between;
-            margin: 10px 0;
-            height: 40px;
+            margin: 0.625em 0;
+            padding: 0.125em 0;
             button {
-              font-family: "Roboto" !important;
+              font-family: inherit;
             }
             .formCreate-things {
-              width: auto;
               border: 1px solid #fff;
               border-radius: 22px;
-              font-size: 18px;
-              padding: 5px 30px;
+              font-size: clamp(1.04rem, 1.04vw, 1.4rem);
+              padding: 0.23em 1.36em;
               background-color: #d9d9d9;
               cursor: pointer;
             }
@@ -77,20 +70,19 @@ const FormEventStyle = styled.div`
           }
 
           .formCreate-time-container {
-            width: 100%;
             display: flex;
+            width: 100%;
             background-color: #d9d9d9;
             border-radius: 16px;
-            padding: 10px 0px;
-            font-size: 18px;
-            margin: 20px 0;
+            padding: 0.625em 0px;
+            margin: 1.25em 0;
             .react-datepicker__input-container {
               display: flex;
               align-items: center;
               justify-content: center;
               input {
-                font-size: 14px;
-                font-family: "Roboto" !important;
+                font-size: clamp(0.83rem, 0.83vw, 1rem);
+                font-family: inherit;
                 text-align: center;
               }
             }
@@ -98,39 +90,40 @@ const FormEventStyle = styled.div`
               width: 44%;
               display: flex;
               align-items: center;
-              padding: 0px 10px;
+              padding: 0px 0.625em;
               .react-datepicker-wrapper {
-                margin-left: 10px;
-                width: 170px;
+                margin-left: 0.625rem;
+                min-width: 10.625em;
                 display: flex;
                 align-items: center;
                 input {
                   width: 100%;
-                  margin: 0px;
-                  padding: 0px 0px;
                   border: none !important;
                   background-color: transparent;
+                  &:focus {
+                    outline: none;
+                  }
                 }
               }
             }
             .formCreate-times {
-              padding: 0px 10px;
+              padding: 0px 0.625em;
               display: flex;
               align-items: center;
               justify-content: space-between;
               .dash {
-                margin-right: 10px;
+                margin-right: 0.625em;
               }
               .react-datepicker-wrapper {
-                width: auto;
                 display: flex;
                 justify-content: center;
                 input {
-                  width: 70%;
-                  margin: 0px;
-                  padding: 0px;
+                  width: 80%;
                   border: none !important;
                   background-color: transparent;
+                  &:focus {
+                    outline: none;
+                  }
                 }
               }
             }
@@ -139,23 +132,23 @@ const FormEventStyle = styled.div`
         .buttonFormCreate {
           display: flex;
           justify-content: flex-end;
-          margin: 10px 0;
+          margin: 0.625em 0;
           cursor: pointer;
           .Cancel {
-            padding: 5px 10px;
+            padding: 0.5em 1em;
+            font-size: clamp(1.04rem, 1.04vw, 1.4rem);
             a {
               text-decoration: none;
-              font-size: 20px;
               color: rgba(0, 0, 0, 0.8);
             }
           }
           .Save {
-            width: 20%;
+            padding: 0.5em 1em;
+            font-size: clamp(1.04rem, 1.04vw, 1.4rem);
             text-align: center;
-            margin-left: 20px;
+            margin-left: 1em;
             background-color: #d9d9d9;
             border-radius: 15px;
-            padding: 5px 10px;
             a {
               text-decoration: none;
             }
--- src/containers/Layout/SideBar/FormTodo/index.style.js
+++ src/containers/Layout/SideBar/FormTodo/index.style.js
@@ -21,12 +21,9 @@ const FormTodoStyle = styled.div`
     align-items: center;
     display: flex;
     .modal-container {
-      width: 35%;
-      max-width: 650px;
-      height: auto;
-      max-height: 600px;
-      border: 1px solid #011627;
-      border-radius: 1em;
+      width: clamp(27.5em, 35%, 40em);
+      border: 1.5px solid #011627;
+      border-radius: 15px;
       background-color: #fdfffc;
       display: flex;
       flex-direction: column;
@@ -34,46 +31,42 @@ const FormTodoStyle = styled.div`
       color: #011627;
 
       .header {
-        height: 100px;
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: 5px;
-        .headerText {
-          font-size: 32px;
-        }
+        padding: 1em;
+        font-size: clamp(1.6rem, 1.6vw, 2rem);
       }
       .content {
         width: 80%;
-        padding: 10px 0px;
-        font-size: 19px;
+        padding: 0.5em 0.25em;
+        font-size: clamp(1rem, 1vw, 1.25rem);
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         .add-form {
-          height: 6vh;
-          width: 100%;
-          margin-bottom: 10px;
+          width: 95%;
+          margin-bottom: 0.55em;
           border: 1px solid #000000;
           border-radius: 15px;
-          padding: 8px 12px;
-          box-sizing: border-box;
-          font-size: 18px;
+          padding: 0.44em 0.66em;
+          font-size: clamp(0.9375rem, 0.9375vw, 1.125rem);
           font-family: inherit;
         }
         .formTodo-address {
           text-align: left;
-          height: 8.2vh;
+          height: 4.6em;
           width: 100%;
           border: 1px solid #000000;
           border-radius: 15px;
           box-sizing: border-box;
-          font-size: 18px;
+          font-size: clamp(0.9375rem, 0.9375vw, 1.125rem);
           textarea {
             height: 100%;
             width: 100%;
-            padding: 6px 10px;
+            padding: 0.375em 0.625em;
+            font-size: clamp(0.9375rem, 0.9375vw, 1.125rem);
             background-color: transparent;
             border: none;
             resize: none;
@@ -89,16 +82,14 @@ const FormTodoStyle = styled.div`
           .formCreate-container-things {
             display: flex;
             justify-content: space-between;
-            margin: 10px 0;
+            margin: 0.625rem 0;
             .formCreate-things {
-              width: auto;
               border: 1px solid #fff;
               border-radius: 22px;
-              font-size: 18px;
-              padding: 5px 30px;
+              font-size: clamp(1.04rem, 1.04vw, 1.4rem);
+              padding: 0.23em 1.36em;
               background-color: #d9d9d9;
               cursor: pointer;
-              font-family: inherit;
             }
             .formCreate-todo {
               background-color: #3f80ea;
@@ -108,7 +99,7 @@ const FormTodoStyle = styled.div`
 
           .formTodo-time-container {
             display: flex;
-            margin: 20px 0;
+            margin: 1.25rem 0;
             font-weight: bold;
             align-items: center;
             .formTodo-duration {
@@ -117,64 +108,96 @@ const FormTodoStyle = styled.div`
               background-color: transparent;
               border: 1px solid #000;
               border-radius: 22px;
-              margin-left: 20px;
+              margin-left: 1.25em;
               width: 100%;
-              padding: 8px 12px;
+              padding: 0.5em 0.75em;
               font-weight: normal;
-              font-size: 16px;
+              font-size: clamp(0.83rem, 0.83vw, 1rem);
             }
             .duration {
               display: flex;
               border-radius: 1em;
               .add-form-duration {
                 border: none;
-                width: 30%;
-                padding: 8px 12px;
-                font-size: 16px;
+                padding: 0.5em 0.75em;
+                font-size: clamp(0.83rem, 0.83vw, 1rem);
                 text-align: end;
+              }
+              .day {
+                width: 30%;
+              }
+              .hour {
+                width: 35%;
+              }
+              .minute {
+                width: 45%;
               }
             }
             label {
-              margin: 0px 5px;
-              font-size: 16px;
+              margin: 0px 0.3125em;
+              font-size: clamp(0.83rem, 0.83vw, 1rem);
               display: flex;
               justify-content: center;
               align-items: center;
             }
-            .formTodo-sche-day {
+            .deadline {
               display: flex;
               align-items: center;
               background-color: #d9d9d9;
               border: 1px solid #000;
               border-radius: 22px;
-              margin-left: 20px;
-              width: 100%;
-              padding: 8px 12px;
+              margin-left: 1.25em;
+              padding: 0.5em 0.75em;
               font-weight: normal;
-              font-size: 18px;
-              .react-datepicker-wrapper {
-                .react-datepicker__input-container {
+              .react-datepicker__input-container {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                input {
+                  font-size: clamp(0.83rem, 0.83vw, 1rem);
+                  font-family: inherit;
+                  text-align: center;
+                }
+              }
+              .formTodo-sche-day {
+                width: 50%;
+                display: flex;
+                align-items: center;
+                .react-datepicker-wrapper {
+                  min-width: 10.625em;
+                  display: flex;
+                  align-items: center;
+                  input {
+                    width: 100%;
+                    border: none !important;
+                    background-color: transparent;
+                    &:focus {
+                      outline: none;
+                    }
+                  }
+                }
+                .material-symbols-outlined {
+                  font-size: clamp(1.46rem, 1.46vw, 1.75rem);
+                }
+              }
+              .formCreate-times {
+                padding: 0px 0.625em;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                .react-datepicker-wrapper {
                   display: flex;
                   justify-content: center;
                   align-items: center;
                   input {
-                    width: 100%;
-                    height: 100%;
-                    border: none;
+                    width: 80%;
+                    border: none !important;
                     background-color: transparent;
-                    text-align: center;
+                    &:focus {
+                      outline: none;
+                    }
                   }
                 }
-              }
-              .material-symbols-outlined {
-                font-size: 28px;
-                margin-right: 5px;
-              }
-            }
-            .formTodo-times {
-              display: flex;
-              .material-symbols-outlined {
-                font-size: 30px;
               }
             }
           }
@@ -183,21 +206,23 @@ const FormTodoStyle = styled.div`
           width: 100%;
           display: flex;
           justify-content: flex-end;
-          margin: 10px 0;
+          margin: 0.625em 0;
           cursor: pointer;
           .Cancel {
-            padding: 5px 10px;
+            padding: 0.5em 1em;
+            font-size: clamp(1.04rem, 1.04vw, 1.4rem);
             a {
               text-decoration: none;
-              font-size: 20px;
               color: rgba(0, 0, 0, 0.8);
             }
           }
           .Save {
-            margin-left: 20px;
+            padding: 0.5em 1em;
+            font-size: clamp(1.04rem, 1.04vw, 1.4rem);
+            text-align: center;
+            margin-left: 1em;
             background-color: #d9d9d9;
             border-radius: 15px;
-            padding: 5px 10px;
             a {
               text-decoration: none;
             }
