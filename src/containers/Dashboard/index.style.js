f9a1da86fc639a25e58816234904ede12c6629e1
TuanAnh225 <tuananhchums23@gmail.com>
2023-06-16 08:24:55 +0700

    Responsive Dashboard2

--- src/containers/Dashboard/index.style.js
+++ src/containers/Dashboard/index.style.js
@@ -4,8 +4,8 @@ const DashBoard = styled.div`
   .container {
     display: grid;
     grid-template-columns: 35% 65%;
-    grid-template-rows: 42vh 42vh;
-    gap: 1em 2em;
+    grid-template-rows: 45% 45%;
+    gap: 15% 2em;
     padding: 0.5em 2em 0 2em;
     font-size: clamp(0.75rem, 1.3vw, 3rem);
     .upComingTask {
