6641f3072a0a81d33cc4adee54cc888f33fa6d0e
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-06-17 09:27:03 +0700

    Create Task Workspace Specific

--- src/containers/Workspace/WorkspaceSpecific/WorkspaceSpecific.jsx
+++ src/containers/Workspace/WorkspaceSpecific/WorkspaceSpecific.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { WorkspaceSpecificStyle } from "./index.style";
+import { WorkspaceSpecificStyle, Color, Percent, Img } from "./index.style";
 
 const WorkspaceSpecific = ({
   setTab,
@@ -11,8 +11,16 @@ const WorkspaceSpecific = ({
     setTab("home");
   };
 
+  const options = {
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
+
   const onClickDelete = (key) => {
-    const newToDoData = workspaces.filter((current) => {
+    const newToDoData = currentWorkspace.teamMems.filter((current) => {
       return current.id !== key;
     });
     setWorkspaces(newToDoData);
@@ -73,7 +81,90 @@ const WorkspaceSpecific = ({
               </div>
             </div>
           </div>
-          <div></div>
+          <div className="specific-task">
+            <div className="table">
+              <div className="col ">
+                <span className="material-symbols-outlined">badge</span>
+                <p className="name">Name</p>
+              </div>
+              <div className="col ">
+                <span className="material-symbols-outlined">
+                  assignment_ind
+                </span>
+                <p className="name">Assignee</p>
+              </div>
+              <div className="col ">
+                <span className="material-symbols-outlined">description</span>
+                <p className="name">Description</p>
+              </div>
+              <div className="col ">
+                <span className="material-symbols-outlined">
+                  event_upcoming
+                </span>
+                <p className="name">Deadline</p>
+              </div>
+              <div className="col ">
+                <span className="material-symbols-outlined">instant_mix</span>
+                <p className="name">Priority</p>
+              </div>
+              <div className="col">
+                <span className="material-symbols-outlined">percent</span>
+                <p className="name">Percent</p>
+              </div>
+              <div className="col ">
+                <span className="material-symbols-outlined">
+                  assignment_turned_in
+                </span>
+                <p className="name">Completed</p>
+              </div>
+            </div>
+            <div>
+              {currentWorkspace.teamMems.map((todo, index) => (
+                <div className="table">
+                  <div className="col">
+                    <span>
+                      <Img src={require("../../../img/" + todo.pjImg)} />
+                    </span>
+                    <p>{todo.pjName}</p>
+                  </div>
+                  <div className="col">
+                    <img
+                      className="avatar"
+                      src={require("../../../img/" + todo.memberAvatar)}
+                      alt="/"
+                    />
+                    <p>{todo.memberName}</p>
+                  </div>
+                  <div className="col">
+                    <p>{todo.description}</p>
+                  </div>
+                  <div className="col">
+                    <p>{todo.deadline.toLocaleString("en-US", options)}</p>
+                  </div>
+                  <div className="col ">
+                    <Color value={todo.level}>Level {todo.level}</Color>
+                  </div>
+                  <div className="col">
+                    <div className="percent">
+                      <Percent percent={todo.percent}>
+                        <div className="percentContent">{todo.percent}</div>
+                      </Percent>
+                    </div>
+                  </div>
+                  <div className="col ">
+                    <span
+                      class="material-symbols-outlined delete"
+                      onClick={() => {
+                        onClickDelete(index);
+                      }}
+                    >
+                      delete
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
           <div className="buttons">
             <button className="add-button" type="button">
               Add task
@@ -109,6 +200,10 @@ const WorkspaceSpecific = ({
                   </div>
                 );
               })}
+          </div>
+          <div className="add-member">
+            <span class="material-symbols-outlined">add_circle</span>
+            <p>Add more user</p>
           </div>
         </div>
       </div>
--- src/containers/Workspace/WorkspaceSpecific/index.style.js
+++ src/containers/Workspace/WorkspaceSpecific/index.style.js
@@ -103,7 +103,87 @@ const WorkspaceSpecificStyle = styled.div`
           }
         }
       }
+      .specific-task {
+        display; flex;
+        width: 65vw;
+        margin-left: -10px;
+        .table {
+          width: 100%;
+          display: flex;
+          font-size: 16px;
+          height: 28px;
+          border-bottom: 2px solid #ccc;
+          align-items: baseline;
+          justify-content: center;
+          padding: 5px 0px 5px 50px;
+          text-align: center;
+          align-items: center;
+          .col {
+            display: flex;
+            align-items: center;
+            text-align: center;
+            flex: 1;
+            margin-left: -30px;
+            margin-right: 30px;
+            span {
+              height: 24px;
+              margin-right: 5px;
+            }
+            p {
+              text-align: center;
+              height: 24px;
+              font-size: 18px;
+              align-items: center;
+              margin: 0px 0px 0px 5px;
+            }
+            .name {
+              font-size: 20px;
+              font-weight: 600;
+            }
+            .avatar {
+              height: 25px;
+              width: 25px;
+              border-radius: 50%;
+              margin-right: 5px;
+            }
+            .done {
+              background-color: #00ff00;
+              padding: 1px 15px 0;
+              border-radius: 20px;
+            }
+            .level {
+              padding: 1px 20px 0;
+              border-radius: 20px;
+            }
+            .sort {
+              margin-left: 5px;
+              cursor: pointer;
+            }
+
+            .check {
+              height: 24px;
+              width: 24px;
+            }
+
+            .delete {
+              cursor: pointer;
+              margin-left: 5px;
+            }
+            .percent {
+              display: flex;
+              justify-content: flex-start;
+              width: 120px;
+              height: 24px;
+              border-radius: 1em;
+              border: 1px solid #ccc;
+              padding: 0px;
+            }
+          }
+        }
+      }
       .buttons {
+        margin-top: 10px;
+        margin-right: 10px;
         display: flex;
         justify-content: flex-end;
         .add-button {
@@ -183,8 +263,69 @@ const WorkspaceSpecificStyle = styled.div`
           }
         }
       }
+      .add-member {
+        display: flex;
+        padding-left: 10px;
+        margin-top: 10px;
+        p {
+          margin: 0;
+          font-size: 18px;
+          font-weight: 300;
+          margin: 0px 10px;
+        }
+      }
     }
   }
 `;
 
-export { WorkspaceSpecificStyle };
+const levelColor = {
+  5: "#FF0000",
+  4: "#FFA500",
+  3: "#FFFF00",
+  2: "#1AC71A",
+  1: "#00BFFF",
+};
+
+const percentToDo = {
+  0: "0%",
+  10: "10%",
+  20: "20%",
+  30: "30%",
+  40: "40%",
+  50: "50%",
+  60: "60%",
+  70: "70%",
+  80: "80%",
+  90: "90%",
+};
+
+const Color = styled.p`
+  padding: 1px 25px 0;
+  border-radius: 20px;
+  background-color: ${({ value }) => levelColor[value]};
+`;
+
+const Percent = styled.p`
+  display: flex;
+  background-color: #2d7fe0;
+  height: 24px;
+  border-radius: 1em;
+  margin: 0px !important;
+  width: ${({ percent }) => percentToDo[percent]};
+  text-align: center;
+  justify-content: center;
+  .percentContent {
+    font-size: 10px;
+    color: #fff;
+    font-weight: 600;
+  }
+`;
+
+const Img = styled.img`
+  text-align: center;
+  align-items: center;
+  height: 24px;
+  width: 22px;
+`;
+
+export { WorkspaceSpecificStyle, Color, Percent, Img };
