8779d4fafc508a6373a54d957d79499ad0f1ecd3
TuanAnh225 <tuananhchums23@gmail.com>
2023-06-25 11:30:37 +0700

    Update data events+users

--- src/containers/Layout/Main/Main.jsx
+++ src/containers/Layout/Main/Main.jsx
@@ -6,6 +6,8 @@ import Dashboard from "../../Dashboard/Dashboard";
 import Todolist from "../../Todolist/Todolist";
 import Workspace from "../../Workspace/Workspace";
 import eventData from "../../../data/events";
+import userData from "../../../data/users";
+
 const Main = ({ tab }) => {
   const [id, setId] = useState(500);
   const [events, setEvents] = useState(
@@ -177,68 +179,71 @@ const Main = ({ tab }) => {
       eventType: "todo",
     },
   ]);
-  const [people, setPeople] = useState([
-    {
-      id: 1,
-      memberName: "Thu thút",
-      memberAvatar: "thu_1.png",
-      pjName: "Python",
-      pjImg: "python.jpg",
-      description: "Coding Homepage",
-      deadline: new Date(2023, 5, 20, 16, 30),
-      percent: "20",
-      level: "3",
-      comple: "false",
-    },
-    {
-      id: 2,
-      memberName: "Bá Hoàng",
-      memberAvatar: "python.jpg",
-      pjName: "Python",
-      pjImg: "python.jpg",
-      description: "Coding Login/Sign up",
-      deadline: new Date(2023, 5, 20, 21, 30),
-      percent: "60",
-      level: "3",
-      comple: "false",
-    },
-    {
-      id: 3,
-      memberName: "Tuấn Anh",
-      memberAvatar: "user6.jpg",
-      pjName: "Python",
-      pjImg: "python.jpg",
-      description: "Learning Design Rules",
-      deadline: new Date(2023, 5, 20, 22, 0),
-      percent: "30",
-      level: "4",
-      comple: "false",
-    },
-    {
-      id: 4,
-      memberName: "Quang Huy",
-      memberAvatar: "user5.jpg",
-      pjName: "Python",
-      pjImg: "python.jpg",
-      description: "Coding Layout",
-      deadline: new Date(2023, 5, 21, 13, 30),
-      percent: "80",
-      level: "2",
-      comple: "false",
-    },
-    {
-      id: 5,
-      memberName: "Vũ Hoàng",
-      memberAvatar: "user7.jpg",
-      pjName: "UI UX",
-      pjImg: "uiux.png",
-      description: "Design admin page",
-      deadline: new Date(2023, 5, 26, 10, 0),
-      percent: "60",
-      level: "2",
-      comple: "false",
-    },
-  ]);
+  const [people, setPeople] = useState(userData);
+  console.log(events);
+  console.log(people);
+  // [
+  //   {
+  //     id: 1,
+  //     memberName: "Thu thút",
+  //     memberAvatar: "thu_1.png",
+  //     pjName: "Python",
+  //     pjImg: "python.jpg",
+  //     description: "Coding Homepage",
+  //     deadline: new Date(2023, 5, 20, 16, 30),
+  //     percent: "20",
+  //     level: "3",
+  //     comple: "false",
+  //   },
+  //   {
+  //     id: 2,
+  //     memberName: "Bá Hoàng",
+  //     memberAvatar: "python.jpg",
+  //     pjName: "Python",
+  //     pjImg: "python.jpg",
+  //     description: "Coding Login/Sign up",
+  //     deadline: new Date(2023, 5, 20, 21, 30),
+  //     percent: "60",
+  //     level: "3",
+  //     comple: "false",
+  //   },
+  //   {
+  //     id: 3,
+  //     memberName: "Tuấn Anh",
+  //     memberAvatar: "user6.jpg",
+  //     pjName: "Python",
+  //     pjImg: "python.jpg",
+  //     description: "Learning Design Rules",
+  //     deadline: new Date(2023, 5, 20, 22, 0),
+  //     percent: "30",
+  //     level: "4",
+  //     comple: "false",
+  //   },
+  //   {
+  //     id: 4,
+  //     memberName: "Quang Huy",
+  //     memberAvatar: "user5.jpg",
+  //     pjName: "Python",
+  //     pjImg: "python.jpg",
+  //     description: "Coding Layout",
+  //     deadline: new Date(2023, 5, 21, 13, 30),
+  //     percent: "80",
+  //     level: "2",
+  //     comple: "false",
+  //   },
+  //   {
+  //     id: 5,
+  //     memberName: "Vũ Hoàng",
+  //     memberAvatar: "user7.jpg",
+  //     pjName: "UI UX",
+  //     pjImg: "uiux.png",
+  //     description: "Design admin page",
+  //     deadline: new Date(2023, 5, 26, 10, 0),
+  //     percent: "60",
+  //     level: "2",
+  //     comple: "false",
+  //   },
+  // ];
   const [workspaces, setWorkspaces] = useState([
     {
       id: 1,
